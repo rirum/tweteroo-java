03bbf5aaeaaac57347f145495dcea880aca6354f
camille <106037608+rirum@users.noreply.github.com>
2024-01-23 17:50:20 -0300

    feat: post user

---
+++ src/main/java/com/tweetroo/api/controllers/UserController.java
@@ -0,0 +1,47 @@
+package com.tweetroo.api.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tweetroo.api.models.UserModel;
+import com.tweetroo.api.repositories.UserRepository;
+
+import lombok.NonNull;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@RestController
+@RequestMapping("/users")
+
+public class UserController {
+    final UserRepository userRepository;
+
+    UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping
+
+    public List<UserModel> getUsers() {
+        return userRepository.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<@NonNull UserModel> createUser(@RequestBody @NonNull UserModel user) {
+        try {
+            UserModel newUser = userRepository.save(user);
+            return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+}
